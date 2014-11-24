e96ec1a910cb7a61834ef3812a89e27b373a1a8a
teledong <teledong@gmail.com>
2014-11-25 01:54:19 +0900

    remove fix patch

--- mono/libgdiplus.rb
+++ mono/libgdiplus.rb
@@ -10,15 +10,6 @@ class Libgdiplus < Formula
   depends_on 'libexif'
   depends_on 'glib'
 
-  # Fix compilation against libpng1.5. See:
-  # https://bugs.gentoo.org/355113
-  # https://bugzilla.novell.com/show_bug.cgi?id=666583
-  def patches
-    { :p0 =>
-    'http://cvsweb.se.netbsd.org/cgi-bin/bsdweb.cgi/~checkout~/pkgsrc/graphics/libgdiplus/patches/patch-aa?rev=1.9;content-type=text%2Fplain'
-    }
-  end if MacOS.lion?
-
   def install
     ENV.x11
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
